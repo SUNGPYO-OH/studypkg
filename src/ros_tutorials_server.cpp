c0db717103c920baf30c52a8dbadba4231d72f09
SUNGPYO-OH <growingp20@gmail.com>
2020-08-10 16:53:53 +0900

    파라미터를 이용한 서비스

--- src/ros_tutorials_server.cpp
+++ src/ros_tutorials_server.cpp
@@ -1,10 +1,30 @@
 #include "ros/ros.h"
 #include "studypkg/tutorial_srv.h"
 
-bool add(studypkg::tutorial_srv::Request &req,
+// PARAMETER SET
+#define PLUS 1
+#define MINUS 2
+#define MULTIPLICATION 3
+#define DIVISION 4
+
+int operator_ = PLUS;
+
+bool calculation(studypkg::tutorial_srv::Request &req,
          studypkg::tutorial_srv::Response &res)
 {
-  res.result = req.a + req.b;
+  switch(operator_)
+  {
+    case PLUS:
+      res.result = req.a + req.b; break;
+    case MINUS:
+      res.result = req.a - req.b; break;
+    case MULTIPLICATION:
+      res.result = req.a * req.b; break;
+    case DIVISION:
+      if(req.b == 0) { ROS_INFO("Wrong Input"); break; }
+      else { res.result = req.a / req.b; break; }
+  }
+  
   ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
   ROS_INFO("sending back response: [%ld]", (long int)res.result);
   return true;
@@ -14,10 +34,17 @@ int main(int argc, char **argv)
 {
   ros::init(argc, argv, "rostutorials_server");
   ros::NodeHandle nh;
-
-  ros::ServiceServer service = nh.advertiseService("ros_tutorial_srv", add);
-  ROS_INFO("Ready to add two ints.");
-  ros::spin();
-
+  nh.setParam("calculation_method", PLUS); // 초기설정 =  PLUS , 파라미터 이름 = "calculation_method"
+  ros::ServiceServer service = nh.advertiseService("ros_tutorial_srv", calculation);
+  ROS_INFO("Ready to Calculation");
+  
+  ros::Rate r(10); // 10Hz 주기설정
+  
+  while (1)
+  {
+    nh.getParam("calculation_method",operator_);
+    ros::spinOnce();
+    r.sleep(); // 무한루프 탈출(루틴반복)
+  }
   return 0;
 }
